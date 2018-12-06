471dff6a0dc794686a18218befd13089769671dc
Erik Peterson <Erik.Exists@gmail.com>
2018-12-06 15:10:14 -0800

    Cleanup init

--- infomercial/discrete/__init__.py
+++ infomercial/discrete/__init__.py
@@ -1,6 +1,3 @@
 from infomercial.discrete import games
 from infomercial.discrete import policy
-
-from infomercial.discrete.information import create_distribution
-from infomercial.discrete.information import update_distribution
-from infomercial.discrete.information import information_value
+from infomercial.discrete.value import information_value
