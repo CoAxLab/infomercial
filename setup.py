527b5abdfa108d75c5dd478def2dadf580aedb3b
Erik Peterson <Erik.Exists@gmail.com>
2018-12-06 15:10:19 -0800

    Docs

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='infomercial',
     version='0.0.1',
-    description="Simulate agents who seek information.",
+    description="Agents who seek information.",
     url='',
     author='Erik J. Peterson',
     author_email='erik.exists@gmail.com',
     license='MIT',
     packages=['infomercial'],
-    scripts=['infomercial/exp/discrete_information_testing.py'],
+    # scripts=['infomercial/exp/discrete_information_testing.py'],
     zip_safe=False)
