7d0e55622d5ceff1a69796d05684e8f89968b89e
Erik Peterson <Erik.Exists@gmail.com>
2019-04-05 13:35:09 -0700

    tune_bandit is running
    (not sure yet it is working correctly.)

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -1,3 +1,4 @@
 from infomercial.exp.meta_bandit import run as meta_bandit
 from infomercial.exp.beta_bandit import run as beta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
+from infomercial.exp.tune_bandit import run as tune_bandit
---
+++ infomercial/exp/tune_bandit.py
@@ -0,0 +1,107 @@
+import fire
+import ray
+from ray.tune import sample_from
+from ray.tune import run as ray_run
+from ray.tune import Trainable
+from ray.tune.schedulers import PopulationBasedTraining
+
+import numpy as np
+from infomercial.exp import meta_bandit
+from infomercial.exp import beta_bandit
+from infomercial.exp import epsilon_bandit
+from infomercial.exp.meta_bandit import save_checkpoint
+from infomercial.exp.meta_bandit import load_checkpoint
+
+from functools import partial
+
+
+class TuneBase(Trainable):
+    def _setup(self, config):
+        self.config = config
+        self.iteration = 0
+        self.result = None
+
+    def _save(self, filename):
+        return save_checkpoint(self.result, filename=filename)
+
+    def _restore(self, filename):
+        return load_checkpoint(filename=filename)
+
+    def reset_config(self, new_config):
+        self.config = new_config
+        return True
+
+
+class TuneBeta(TuneBase):
+    def _train(self):
+        self.result = beta_bandit(**self.config)
+        self.iteration += 1
+        return self.result
+
+
+class TuneMeta(TuneBase):
+    def _train(self):
+        self.result = meta_bandit(**self.config)
+        self.iteration += 1
+        return self.result
+
+
+class TuneEpsilon(TuneBase):
+    def _train(self):
+        self.result = epsilon_bandit(**self.config)
+        self.iteration += 1
+        return self.result
+
+
+def run(name,
+        env_name='BanditOneHigh10-v0',
+        exp_name='beta_bandit',
+        num_samples=10,
+        **config_kwargs):
+    """Tune hyperparameters of any bandit experiment."""
+
+    # Build the config dict
+    config = {}  # Home for processed kwargs
+    keys = sorted(list(config_kwargs.keys()))
+    for k in keys:
+        # Either it is a param list, so make it a sampling fn
+        try:
+            begin, end = config_kwargs[k]
+            v = lambda spec: np.random.uniform(begin, end)
+            v = sample_from(v)
+        # Or a scalar
+        except TypeError:
+            v = config_kwargs[k]
+
+        config[k] = v
+
+    if exp_name == 'beta_bandit':
+        Tuner = TuneBeta
+    elif exp_name == 'epsilon_bandit':
+        Tuner = TuneEpsilon
+    elif exp_name == 'meta_bandit':
+        Tuner = TuneMeta
+    else:
+        raise ValueError("exp_name not known")
+
+    pbt = PopulationBasedTraining(
+        time_attr='training_iteration',
+        reward_attr='total_R',
+        perturbation_interval=600.0,
+        hyperparam_mutations=config)
+
+    # stop = {"num_samples": num_samples}
+    ray_run(
+        Tuner,
+        local_dir=name,
+        num_samples=num_samples,
+        name=name,
+        reuse_actors=True,
+        # stop=stop,
+        config=config,
+        scheduler=pbt,
+        verbose=False)
+
+
+if __name__ == "__main__":
+    fire.Fire(run)
