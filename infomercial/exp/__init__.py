272f6cfbf66581a4efcf539000e511ddc517d2d4
Erik Peterson <Erik.Exists@gmail.com>
2019-04-17 09:52:35 -0700

    tune_replicator runs; needs  testing

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -3,3 +3,4 @@ from infomercial.exp.beta_bandit import run as beta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
 from infomercial.exp.tune_bandit import tune_random
 from infomercial.exp.tune_bandit import tune_pbt
+from infomercial.exp.tune_bandit import tune_replicator
--- infomercial/exp/tune_bandit.py
+++ infomercial/exp/tune_bandit.py
@@ -35,6 +35,17 @@ def get_best_result(trial_list, metric):
     return {metric: get_best_trial(trial_list, metric)[metric]}
 
 
+def get_configs(trial_list):
+    """Extract configs"""
+
+    return [trial["config"] for trial in trial_list]
+
+
+def get_metrics(trial_list, metric):
+    """Extract metric"""
+    return [trial[metric] for trial in trial_list]
+
+
 def train(exp_func=None,
           env_name=None,
           num_episodes=None,
@@ -142,7 +153,6 @@ def tune_random(name,
 def tune_pbt(name,
              exp_name='beta_bandit',
              env_name='BanditOneHigh10-v0',
-             top_threshold=0.25,
              num_iterations=2,
              num_episodes=2000,
              num_samples=10,
@@ -153,6 +163,7 @@ def tune_pbt(name,
              **config_kwargs):
     """Tune hyperparameters of any bandit experiment."""
     prng = np.random.RandomState(seed_value)
+    top_threshold = 0.5  # Fix to hold pop size const, as per PBT.
 
     # ------------------------------------------------------------------------
     # Init:
@@ -279,6 +290,174 @@ def tune_pbt(name,
     return best, trials
 
 
+def tune_replicator(name,
+                    exp_name='beta_bandit',
+                    env_name='BanditOneHigh10-v0',
+                    num_iterations=2,
+                    num_episodes=2000,
+                    num_replicators=10,
+                    num_processes=1,
+                    perturbation=0.1,
+                    extend_episodes=False,
+                    verbose=False,
+                    seed_value=None,
+                    **config_kwargs):
+    """Tune hyperparameters of any bandit experiment."""
+    prng = np.random.RandomState(seed_value)
+
+    # ------------------------------------------------------------------------
+    # Init:
+    # Separate name from path
+    path, name = os.path.split(name)
+
+    # Look up the bandit run function were using in this tuning.
+    exp_func = getattr(exp, exp_name)
+
+    # Build the parallel callback
+    trials = []
+
+    def append_to_results(result):
+        trials.append(result)
+
+    # Setup default params
+    params = dict(
+        exp_func=exp_func,
+        env_name=env_name,
+        num_episodes=num_episodes,
+        seed_value=seed_value,
+        config={})
+
+    # ------------------------------------------------------------------------
+    # Run first population!
+    population = np.ones(num_replicators) / num_replicators
+
+    #
+    # Setup the parallel workers
+    workers = []
+    pool = Pool(processes=num_processes)
+    for t in range(num_replicators):
+
+        # Reset param sample for safety
+        params["config"] = {}
+
+        # Make a new sample
+        for k, (low, high) in config_kwargs.items():
+            params["config"][k] = prng.uniform(low=low, high=high)
+
+        # A worker gets the new sample
+        workers.append(
+            pool.apply_async(
+                train, kwds=deepcopy(params), callback=append_to_results))
+
+    # Get the worker's result (blocks until complete)
+    for worker in workers:
+        worker.get()
+    pool.close()
+    pool.join()
+    pool.terminate()
+
+    # ------------------------------------------------------------------------
+    # Optimize for num_iterations
+    for _ in range(num_iterations):
+        # --------------------------------------------------------------------
+        # Replicate!
+        #
+        # Extract configs from trials
+        configs = get_configs(trials)
+
+        # Get fitness and avg fitness (F_Bar)
+        F = get_metrics(trials, "total_R")
+        F_bar = np.mean(F)
+
+        # Replicate based on the fitness gradient
+        population = (population * F) / F_bar
+        population /= np.sum(population)
+
+        # Cull replicators than chance
+        cull = population >= (1 / population.size)
+        population = population[cull]
+        population /= np.sum(population)
+
+        configs = [configs[c] for c in cull]
+
+        # Replace the culled; Perturb to reproduce.
+        num_children = int(num_replicators - population.size)
+        children = []
+        children_configs = []
+        for n in range(num_children):
+            # Pick a random replicator ith, sampling based on its pop value
+            ith = prng.choice(range(population.size), p=population)
+
+            # Perturb ith config.
+            children_config = configs[ith]
+            for key, value in children_config.items():
+                delta = value * perturbation
+                children_config[key] = prng.uniform(value - delta,
+                                                    value + delta)
+            children_configs.append(children_config)
+
+            # Copy ith p.
+            children.append(population[ith])
+
+        # Update population w/ children
+        population = np.concatenate([population, children])
+        configs.append(children_configs)
+
+        # Renorm after reproduction
+        population /= np.sum(population)
+
+        # -------------------------------------------------------------------
+        # Run!
+        #
+        # Reset trials for the next round
+        trials = []
+
+        # Re-init the pool
+        workers = []
+        pool = Pool(processes=num_processes)
+
+        # Do another round of opt, with the new config
+        for t in range(population.size):
+            # Reset param sample for safety
+            params["config"] = configs[t]
+
+            # A worker gets the new sample
+            workers.append(
+                pool.apply_async(
+                    train, kwds=deepcopy(params), callback=append_to_results))
+
+        # Get the worker's result (blocks until complete)
+        for worker in workers:
+            worker.get()
+        pool.close()
+        pool.join()
+        pool.terminate()
+
+    # ------------------------------------------------------------------------
+    # Save configs and total_R (full model data is dropped):
+    best = get_best_trial(trials, 'total_R')
+
+    # Best trial config
+    best_config = best["config"]
+    best_config.update(get_best_result(trials, 'total_R'))
+    save_checkpoint(
+        best_config, filename=os.path.join(path, name + "_best.pkl"))
+
+    # Sort and save the configs of all trials
+    sorted_configs = {}
+    for i, trial in enumerate(get_sorted_trials(trials, 'total_R')):
+        sorted_configs[i] = trial["config"]
+        sorted_configs[i].update({"total_R": trial["total_R"]})
+    save_checkpoint(
+        sorted_configs, filename=os.path.join(path, name + "_sorted.pkl"))
+
+    return best, trials
+
+
 if __name__ == "__main__":
     # Generate CL interface.
-    fire.Fire({"random": tune_random, "pbt": tune_pbt})
+    fire.Fire({
+        "random": tune_random,
+        "pbt": tune_pbt,
+        "replicator": tune_replicator
+    })
