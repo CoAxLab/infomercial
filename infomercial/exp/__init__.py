806a4140f1f1aadf8150a259f2a94f78700dcd13
Erik Peterson <Erik.Exists@gmail.com>
2019-06-05 13:35:36 -0700

    Expore random_bandit

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -1,4 +1,5 @@
 from infomercial.exp.meta_bandit import run as meta_bandit
+from infomercial.exp.random_bandit import run as random_bandit
 from infomercial.exp.beta_bandit import run as beta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
 from infomercial.exp.tune_bandit import tune_random
