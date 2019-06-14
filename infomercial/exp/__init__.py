2588b349d46664523b9ed1ca020bf3ece16fdb3e
Erik Peterson <Erik.Exists@gmail.com>
2019-06-14 15:06:30 -0700

    Export softbeta

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -1,6 +1,7 @@
 from infomercial.exp.meta_bandit import run as meta_bandit
 from infomercial.exp.random_bandit import run as random_bandit
 from infomercial.exp.beta_bandit import run as beta_bandit
+from infomercial.exp.softbeta_bandit import run as softbeta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
 from infomercial.exp.tune_bandit import tune_random
 from infomercial.exp.tune_bandit import tune_pbt
