f4977425ce7aa6462b2dba18da75de8459c88d33
Erik Peterson <Erik.Exists@gmail.com>
2020-02-06 16:36:05 -0800

    Export novelty_bandit

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -3,6 +3,7 @@ from infomercial.exp.random_bandit import run as random_bandit
 from infomercial.exp.beta_bandit import run as beta_bandit
 from infomercial.exp.softbeta_bandit import run as softbeta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
+from infomercial.exp.novelty_bandit import run as novelty_bandit
 from infomercial.exp.tune_bandit import tune_random
 from infomercial.exp.tune_bandit import tune_pbt
 from infomercial.exp.tune_bandit import tune_replicator
