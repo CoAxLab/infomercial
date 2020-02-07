472d1929b5f551255d7bfa114f5a56d1a431889c
Erik Peterson <Erik.Exists@gmail.com>
2020-02-06 16:37:28 -0800

    Add novelty_bandit to setup.py. It becomes a CL program!

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup
 
-setup(
-    name='infomercial',
-    version='0.0.1',
-    description="Agents who seek information.",
-    url='',
-    author='Erik J. Peterson',
-    author_email='erik.exists@gmail.com',
-    license='MIT',
-    packages=['infomercial'],
-    scripts=[
-        'infomercial/exp/meta_bandit.py', 'infomercial/exp/epsilon_bandit.py',
-        'infomercial/exp/beta_bandit.py', 'infomercial/exp/softbeta_bandit.py',
-        'infomercial/exp/random_bandit.py', 'infomercial/exp/tune_bandit.py'
-    ],
-    zip_safe=False)
+setup(name='infomercial',
+      version='0.0.1',
+      description="Agents who seek information.",
+      url='',
+      author='Erik J. Peterson',
+      author_email='erik.exists@gmail.com',
+      license='MIT',
+      packages=['infomercial'],
+      scripts=[
+          'infomercial/exp/meta_bandit.py',
+          'infomercial/exp/epsilon_bandit.py',
+          'infomercial/exp/beta_bandit.py',
+          'infomercial/exp/softbeta_bandit.py',
+          'infomercial/exp/random_bandit.py', 'infomercial/exp/tune_bandit.py',
+          'infomercial/exp/novelty_bandit.py'
+      ],
+      zip_safe=False)
