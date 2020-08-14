833f3afc0b4f9911bd6d3b4199df02517f1429c1
Erik Peterson <Erik.Exists@gmail.com>
2020-08-13 18:15:47 -0700

    Move common funcs for RmD analysis to a lib

---
+++ notebooks/utils.R
@@ -0,0 +1,39 @@
+load_result <- function(exp_name, param_codes, run_codes, file_names, 
+                        skip=0, n_max=Inf){
+  runtmp <- NULL
+  tmp <- NULL
+  num_files <- length(file_names)
+  result <- NULL
+  for (param in param_codes){
+    for (run in run_codes) {
+      runtmp <- read_csv(
+        paste(
+          data_path, exp_name, 
+          paste("param", param, sep=""), 
+          paste("run", run, sep=""), 
+          paste(file_names[1], "csv", sep="."), sep="/"),
+        skip=skip,
+        n_max=n_max)    
+      for (name in file_names[2:num_files]){
+        file_name <- paste(data_path, 
+                           exp_name, 
+                           paste("param", param, sep=""), 
+                           paste("run", run, sep=""), 
+                           paste(name, "csv", sep="."), sep="/")
+        if (file.exists(file_name)) {
+          tmp <- read_csv(file_name, skip=skip, n_max=n_max)
+          runtmp[[name]] <- tmp[[name]]
+        } else {
+          warning(
+            paste(file_name, " does not exist (filling with 0).\n", sep=""))
+          runtmp[[name]] <- 0
+        }
+      }
+      runtmp$run <- run
+      runtmp$param <- param
+      runtmp$t <- NULL  # No need to keep the wall time
+      result <- bind_rows(result, runtmp)  
+    }
+  }
+  result
+}
