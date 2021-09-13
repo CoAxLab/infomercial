d8b52e81cb676ce73171309dda2fef6697fc50ac
Erik Peterson <Erik.Exists@gmail.com>
2021-09-13 14:18:26 -0700

    fix: remove import for del submodule

--- infomercial/exp/__init__.py
+++ infomercial/exp/__init__.py
@@ -3,7 +3,6 @@ from infomercial.exp.softwsls_bandit import run as softwsls_bandit
 from infomercial.exp.random_bandit import run as random_bandit
 from infomercial.exp.softcount_bandit import run as softcount_bandit
 from infomercial.exp.softentropy_bandit import run as softentropy_bandit
-from infomercial.exp.beta_bandit import run as beta_bandit
 from infomercial.exp.softbeta_bandit import run as softbeta_bandit
 from infomercial.exp.epsilon_bandit import run as epsilon_bandit
 from infomercial.exp.curiosity_bandit import run as curiosity_bandit
